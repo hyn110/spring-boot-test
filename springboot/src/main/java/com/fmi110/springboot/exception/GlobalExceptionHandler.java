f5ff49be5e8e03396582d1be44bc717b7cdc752c
huangyunning <huangyunning110@gmail.com>
2018-01-20 15:45:51 +0800

    7 添加全局统一异常处理 GobalExceptionHandler

---
+++ springboot/src/main/java/com/fmi110/springboot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.fmi110.springboot.exception;
+
+import com.fmi110.springboot.dto.ErrorInfo;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 全局异常处理类
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    public static final String DEFAULT_ERROR_VIEW = "error";
+
+    /**
+     * 返回错误页面
+     * @param req
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("exception", e);
+        mv.addObject("url", req.getRequestURL());
+        mv.setViewName(DEFAULT_ERROR_VIEW);
+        return mv;
+    }
+
+    /**
+     * 返回json格式的错误信息
+     * @param req
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = ArithmeticException.class)  // 指定处理的异常类型
+    @ResponseBody
+    public ErrorInfo<String> defaultAjaxErrorHandler(HttpServletRequest req, Exception e) {
+        ErrorInfo<String> dto = new ErrorInfo<>();
+        dto.setUrl(req.getRequestURL()
+                      .toString());
+        dto.setData(e.getMessage());
+        dto.setCode(ErrorInfo.ERROR);
+        return dto;
+    }
+}
--- springboot/src/main/java/com/fmi110/springboot/web/HelloController.java
+++ springboot/src/main/java/com/fmi110/springboot/web/HelloController.java
@@ -2,6 +2,7 @@ package com.fmi110.springboot.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,5 +17,13 @@ public class HelloController {
     public String index(ModelMap map) {
         map.addAttribute("host", "www.fmi110.com");
         return "/index";
+    }
+
+    @GetMapping("/exception")
+    @ResponseBody
+    public String exception(){
+//        throw new RuntimeException("故意抛个一个异常...");
+        int a= 1/0;
+        return "异常统一处理演示";
     }
 }
